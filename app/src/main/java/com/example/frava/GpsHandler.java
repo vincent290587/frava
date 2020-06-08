9e184db21af94a8f1475fa517e23b04c608521f7
Vincent Golle <v.golle@isispace.nl>
2020-06-08 16:34:59 +0200

    App runs

---
+++ app/src/main/java/com/example/frava/GpsHandler.java
@@ -0,0 +1,398 @@
+package com.example.frava;
+
+import android.app.Service;
+import android.content.Intent;
+import android.icu.util.TimeZone;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import io.swagger.client.model.LatLng;
+import no.nordicsemi.android.ble.data.Data;
+
+public class GpsHandler extends Service {
+
+    private static final String TAG = "GpsHandler";
+
+    private int nb_segments_sent;
+    MutableLiveData<String> gps_status;
+
+    // Binder given to clients
+    private final IBinder binder = new GpsHandler.LocalBinder();
+
+    List<ExtendedSummarySegment> m_seg_list;
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        GpsHandler getService() {
+            // Return this instance of GpsHandler so clients can call public methods
+            return GpsHandler.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public GpsHandler() {
+        gps_status = new MutableLiveData<>();
+    }
+
+    public void subscribe(Observer<String> observer) {
+
+        gps_status.observeForever(observer);
+    }
+
+    public void handleCommand(@NonNull Data data, LeCallbacks interf) {
+
+        IncomingCommands command = IncomingCommands.fromByte(data.getByte(0));
+
+        Log.i(TAG, "Received GPS command " + data.getByte(0));
+
+        switch (command) {
+
+            case RequestPhoneStatus:
+                sendPhoneStatus(interf);
+                break;
+
+            case GPSReadyToReceiveSegmentList:
+                sendSegmentsList(interf);
+                break;
+
+            case SegmentFileRequest:
+                handleSegmentRequest(data, interf);
+                break;
+
+            default:
+                break;
+        }
+        
+    }
+
+    public void handleSegmentRequest(@NonNull Data data, LeCallbacks interf) {
+
+        if (m_seg_list == null) {
+            return;
+        }
+
+        Log.i(TAG, "handleSegmentRequest");
+
+        // get ID
+        byte[] val = data.getValue();
+        ByteBuffer wrap = ByteBuffer.wrap(val);
+        wrap.order(ByteOrder.LITTLE_ENDIAN);
+
+        long seg_id = wrap.getLong(1);
+
+        for (ExtendedSummarySegment ext_seg : m_seg_list) {
+
+            if (seg_id == ext_seg.summarySegment.getId()) {
+
+                // send whole segment
+                _sendSegment(ext_seg, interf);
+                return;
+            }
+        }
+
+        Log.e(TAG, "handleSegmentRequest: not found " + seg_id);
+    }
+
+    private void _sendSegment(ExtendedSummarySegment ext_seg, LeCallbacks interf) {
+
+        List<byte[]> l_data = new ArrayList<>();
+
+        Log.i(TAG, "_sendSegment");
+
+        nb_segments_sent += 1;
+
+        gps_status.postValue("Nb segments sent: " + nb_segments_sent);
+
+        byte[] ext_seg_bytes = ext_seg.toByteBuffer();
+
+        ByteBuffer wrap = ByteBuffer.wrap(newPacket());
+        wrap.order(ByteOrder.LITTLE_ENDIAN);
+
+        wrap.put(OutgoingCommands.SegmentFileUploadStart.byteValue());
+        wrap.putLong(ext_seg.summarySegment.getId());
+
+        short s = ext_seg.size;
+        wrap.putInt(s);
+
+        // add to list
+        l_data.add(wrap.array());
+
+        int i = 0;
+        while (true) {
+            int i2 = s - i;
+            if (i2 > 19) {
+                ByteBuffer wrap2 = ByteBuffer.wrap(newPacket());
+                wrap2.order(ByteOrder.LITTLE_ENDIAN);
+
+                wrap2.put(OutgoingCommands.SegmentFileUploadData.byteValue());
+                wrap2.put(ext_seg_bytes, i, 19);
+                l_data.add(wrap2.array());
+                i += 19;
+            } else {
+                ByteBuffer wrap3 = ByteBuffer.wrap(newPacket());
+                wrap3.order(ByteOrder.LITTLE_ENDIAN);
+
+                wrap3.put(OutgoingCommands.SegmentFileUploadEnd.byteValue());
+                wrap3.put(ext_seg_bytes, i, i2);
+                l_data.add(wrap3.array());
+
+                // send the data
+                if (interf != null) {
+                    interf.onListDataSend(l_data);
+                }
+                return;
+            }
+        }
+
+    }
+
+    public void sendPhoneStatus(LeCallbacks interf) {
+
+        byte[] data = new byte[20];
+        ByteBuffer wrap = ByteBuffer.wrap(data);
+        wrap.order(ByteOrder.LITTLE_ENDIAN);
+
+        wrap.put(OutgoingCommands.PhoneStatus.byteValue());
+        wrap.put((byte) 66);
+        wrap.put((byte) ((int) ((((float) (TimeZone.getDefault().getOffset(new Date().getTime()) / 60000)) / 60.0f) * 4.0f)));
+        wrap.put((byte) 0);
+        wrap.put((byte) 1);
+
+        Log.i(TAG, "sendPhoneStatus");
+
+        // send the data
+        if (interf != null) {
+            interf.onDataSend(data);
+        }
+    }
+
+    public void sendSegmentsListReady(List<ExtendedSummarySegment> seg_list, LeCallbacks interf) {
+
+        byte[] data = new byte[20];
+        ByteBuffer wrap = ByteBuffer.wrap(data);
+        wrap.order(ByteOrder.LITTLE_ENDIAN);
+
+        m_seg_list = seg_list;
+
+        nb_segments_sent = 0;
+
+        wrap.put(OutgoingCommands.NewSegmentListReady.byteValue());
+        if (seg_list == null) {
+            wrap.put((byte) 0);
+        } else {
+            wrap.put((byte) seg_list.size());
+        }
+
+        Log.i(TAG, "sendSegmentsListReady");
+
+        gps_status.postValue("sendSegmentsListReady");
+
+        // send the data
+        if (interf != null) {
+            interf.onDataSend(data);
+        }
+    }
+
+    private void sendSegmentsList(LeCallbacks interf) {
+
+        int counter = 1;
+
+        if (m_seg_list == null) {
+            return;
+        }
+
+        Log.i(TAG, "sendSegmentsList");
+
+        gps_status.postValue("sendSegmentsList");
+
+        List<byte[]> l_data = new ArrayList<>();
+
+        for (ExtendedSummarySegment ext_seg : m_seg_list) {
+
+            byte[] data = new byte[20];
+            ByteBuffer wrap = ByteBuffer.wrap(data);
+            wrap.order(ByteOrder.LITTLE_ENDIAN);
+
+            if (counter == m_seg_list.size()) {
+                wrap.put(OutgoingCommands.SegmentListItemDone.byteValue());
+            } else {
+                wrap.put(OutgoingCommands.SegmentListItem.byteValue());
+            }
+
+            wrap.putLong(ext_seg.summarySegment.getId());
+
+            LatLng latlng = ext_seg.summarySegment.getStartLatlng();
+            wrap.putInt((int) (latlng.get(0) * 1.1930464E7d));
+            wrap.putInt((int) (latlng.get(1) * 1.1930464E7d));
+            wrap.putChar((char) 0);
+            wrap.put((byte) 0);
+
+            l_data.add(data);
+
+            counter += 1;
+        }
+
+        // send the data
+        if (interf != null) {
+            interf.onListDataSend(l_data);
+        }
+
+    }
+
+
+    public enum OutgoingCommands {
+        InvalidCommand(0),
+        RequestFitFileList(1),
+        RequestFitFileDownload(2),
+        RequestFitFileDelete(3),
+        NewSegmentListReady(4),
+        EmailNotification(5),
+        EmailNotificationBody(6),
+        EmailNotificationNext(7),
+        SMSNotification(8),
+        SMSNotificationBody(9),
+        SMSNotificationNext(10),
+        CallNotification(11),
+        RequestSlowDownloads(12),
+        NavigationNewRoute(13),
+        NavigationRouteDataContinue(14),
+        NavigationStep(15),
+        NavigationStepContinued(16),
+        NavigationRouteData(17),
+        SegmentListItem(18),
+        SegmentListItemDone(19),
+        SegmentFileUploadStart(20),
+        SegmentFileUploadData(21),
+        SegmentFileUploadEnd(22),
+        PhoneStatus(25),
+        SegmentUpdateCancel(26),
+        NavigationError(27),
+        NavigationNewDestination(29),
+        NavigationNewFile(30),
+        NavigationNewFileDest(31),
+        NavFileUploadDataStart(32),
+        NavFileUploadData(33),
+        NavFileUploadEnd(34),
+        NavigationPhoneCancel_V2(35),
+        SettingsRequest(36),
+        UpdateSetting(37),
+        UpdateSettingContinue(38),
+        Notification(39),
+        NotificationBody(40),
+        NotificationNext(41),
+        TrainingFileUploadStart(42),
+        TrainingFileUploadData(43),
+        TrainingFileUploadEnd(44),
+        MapFileReadyStart(45),
+        MapFileReadyEnd(46),
+        MapFileStart(47),
+        MapFileData(48),
+        MapFileEnd(49),
+        MapFileError(50),
+        MapFileListRequest(51),
+        MapFileDelete(52);
+
+        public int value;
+
+        OutgoingCommands(int i) {
+            this.value = i;
+        }
+
+        public byte byteValue() {
+            return (byte) this.value;
+        }
+
+        public static OutgoingCommands fromByte(byte b) {
+            for (OutgoingCommands outgoingCommands : values()) {
+                if (outgoingCommands.byteValue() == b) {
+                    return outgoingCommands;
+                }
+            }
+            return null;
+        }
+    }
+
+    public enum IncomingCommands {
+        InvalidCommand(0),
+        FitFileTransferStart(1),
+        FitFileTransferData(2),
+        FitFileTransferEnd(3),
+        SwitchingToHighSpeed(4),
+        ConnectedInHighSpeed(5),
+        SwitchingToLowSpeed(6),
+        ConnectedInLowSpeed(7),
+        DebugText(8),
+        ErrorPacket(9),
+        FileDeleteConfirmation(10),
+        FileListSending(11),
+        NavigationRerouteRequest(12),
+        LiveSensorData(13),
+        LiveSessionStatus(14),
+        GPSReadyToReceiveSegmentList(15),
+        RequestPhoneStatus(16),
+        SegmentFileRequest(17),
+        SegmentFileRequestEnd(18),
+        NavigationNewFileReady(20),
+        SettingsSendStart(22),
+        NavigationGPSCancel(23),
+        SettingsSendData(24),
+        SettingsSendError(25),
+        TrainingSendError(27),
+        MapFileReq(28),
+        MapFileError(29),
+        RequestMTUUpdate(30),
+        MapFileSizeError(31),
+        MapFileListData(32),
+        MapFileListEnd(33),
+        MapFileDeleteComplete(34),
+        MapFileDeleteError(35),
+        MapFileListError(36),
+        NavigationFailed(37);
+
+        public int value;
+
+        public byte byteValue() {
+            return (byte) this.value;
+        }
+
+        IncomingCommands(int i) {
+            this.value = i;
+        }
+
+        public static IncomingCommands fromByte(byte b) {
+            for (IncomingCommands incomingCommands : values()) {
+                if (incomingCommands.byteValue() == b) {
+                    return incomingCommands;
+                }
+            }
+            return null;
+        }
+    }
+
+    private byte[] newPacket() {
+        byte[] bArr = new byte[20];
+        for (int i = 0; i < bArr.length; i++) {
+            bArr[i] = 0;
+        }
+        return bArr;
+    }
+
+}
---
+++ app/src/main/java/com/example/frava/OAuthStarter.java
@@ -0,0 +1,263 @@
+package com.example.frava;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class OAuthStarter extends AppCompatActivity {
+
+    public static final int OAUTH_FIRST_REQUEST_CODE = 1113;
+    private static final String TAG = "OAuthStarter";
+    RequestQueue queue;
+
+    public OAuthStarter() {
+
+    }
+
+    private void logText(String s) {
+        final TextView textview = findViewById(R.id.text_dashboard);
+
+        if (textview != null) {
+
+            CharSequence orig = textview.getText();
+            textview.setText(s);
+            textview.append(orig);
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        queue = Volley.newRequestQueue(this);  // this = context
+
+        Log.d(TAG, "onCreate");
+
+        connectWithStrava();
+
+        //goBackToMain();
+    }
+
+    private void goBackToMain() {
+
+        // we are done, go back to the main
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Code", MainActivity.INTENT_OAUTH2_DONE);
+        startActivity(intent);
+    }
+
+    private void connectWithStrava() {
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String refresh_token = sharedPref.getString(getString(R.string.refresh_token), "");
+        int expires_at = sharedPref.getInt(getString(R.string.expires_at), 0);
+        long timestamp = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+
+        Log.i(TAG, "connectWithStrava: " + refresh_token);
+
+        if (refresh_token == null || refresh_token.isEmpty() || expires_at == 0) {
+            // authorize app through browser
+            connectWithStravaFirstTime();
+        } else if ((long)expires_at < timestamp) {
+            // ask for a new token
+            askForNewToken(refresh_token, expires_at);
+        } else {
+            goBackToMain();
+        }
+
+    }
+
+    private void connectWithStravaFirstTime() {
+
+        Log.i(TAG, "connectWithStravaFirstTime");
+
+        Uri intentUri = Uri.parse("https://www.strava.com/oauth/mobile/authorize")
+                .buildUpon()
+                .appendQueryParameter("client_id", "2940")
+                .appendQueryParameter("redirect_uri", "xcomputer://xcomputer")
+                .appendQueryParameter("response_type", "code")
+                .appendQueryParameter("approval_prompt", "auto")
+                .appendQueryParameter("scope", "profile:read_all,activity:write")
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
+        startActivityForResult(intent, OAUTH_FIRST_REQUEST_CODE);
+    }
+
+    private void askForNewToken(final String refresh_token, final int expires_at) {
+
+        Log.i(TAG, "askForNewToken");
+
+        logText("Requesting new token" + '\n');
+
+        String url = "https://www.strava.com/oauth/token";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // parse
+                        try {
+                            JSONObject jobj = null;
+                            jobj = new JSONObject(response);
+                            Log.i(TAG, jobj.toString(1));
+
+                            final String access_token = jobj.getString("access_token");
+                            final String refresh_token = jobj.getString("refresh_token");
+                            final int expires_at = jobj.getInt("expires_at");
+
+                            storeKeys(access_token, refresh_token, expires_at);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("client_id", "2940");
+                params.put("client_secret", "0670dd68e47561cb9b91b4a2751af1df0f1d708d");
+                params.put("grant_type", "refresh_token");
+                params.put("refresh_token", refresh_token);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+
+    private void storeKeys(final String access_token, final String refresh_token, final int expires_at) {
+
+        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = m.edit();
+
+        editor.putString(getString(R.string.access_token), access_token);
+        Log.i(TAG, "Access: " + access_token);
+
+        editor.putString(getString(R.string.refresh_token), refresh_token);
+        Log.i(TAG, "Refresh: " + refresh_token);
+
+        editor.putInt(getString(R.string.expires_at), expires_at);
+        Log.i(TAG, "Expires_at: " + expires_at);
+
+        editor.commit();
+
+        logText("Storing key" + '\n');
+
+        // we are done, go back to the main
+        goBackToMain();
+    }
+
+    private void performOAuthStep2(final String code) {
+
+        Log.i(TAG, "performOAuthStep2");
+        Log.i(TAG, code);
+
+        logText("OAuth running" + '\n');
+
+        String url = "https://www.strava.com/oauth/token";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        //Log.d("Response", response);
+
+                        // parse
+                        try {
+                            JSONObject jobj = null;
+                            jobj = new JSONObject(response);
+                            Log.i(TAG, jobj.toString(1));
+
+                            String access_token = jobj.getString("access_token");
+                            String refresh_token = jobj.getString("refresh_token");
+                            final int expires_at = jobj.getInt("expires_at");
+
+                            storeKeys(access_token, refresh_token, expires_at);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("client_id", "2940");
+                params.put("client_secret", "0670dd68e47561cb9b91b4a2751af1df0f1d708d");
+                params.put("code", code);
+                params.put("grant_type", "authorization_code");
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+
+    // Call Back method  to get the Message form other Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "onActivityResult " + requestCode + "  code=" + resultCode);
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onResume() {
+        // The following line will return "appSchema://appName.com".
+        super.onResume();
+        String CALLBACK_URL = "xcomputer://xcomputer";
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            Log.d(TAG, "onResume " + getIntent().getData().toString());
+            Log.d(TAG, "URI " + uri.toString());
+            if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
+                final String access_token = uri.getQueryParameter("code");
+
+                // start second auth step
+                performOAuthStep2(access_token);
+            }
+        }
+        // Perform other operations here.
+    }
+}
